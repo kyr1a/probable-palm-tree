98ee7b33d154aec7db934c746f20d56b5697cdc2
kyria <work.rebecaoliveira@gmail.com>
2025-06-15 19:04:32 -0300

    exercicio typescript

---
+++ multiplicacao-ola.ts
@@ -0,0 +1,12 @@
+function multiplicacao(a: number, b: number): number {
+  return a * b;
+}
+
+function saudacao(nome: string): string {
+  return "Olá " + nome;
+}
+
+console.log(multiplicacao(5, 3));
+console.log(multiplicacao(2.5, 4));
+console.log(saudacao("Maria"));
+console.log(saudacao("João"));
